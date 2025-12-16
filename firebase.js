c090a1b14182eeae1819cc4426106e0d3074bc4c
githubmaster7 <98905838+githubmaster7@users.noreply.github.com>
2025-12-15 20:08:37 -0600

    Add Firebase initialization in firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCA49-fMqK4vmtcVs1kvoKPsryt8otwvQA",
+  authDomain: "micds-assessments.firebaseapp.com",
+  projectId: "micds-assessments",
+  storageBucket: "micds-assessments.firebasestorage.app",
+  messagingSenderId: "104930143238",
+  appId: "1:104930143238:web:f8efeacdf297a406822787"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
