1e8edab292d8f664342440093f0c0a5f029b3b51
githubmaster7 <98905838+githubmaster7@users.noreply.github.com>
2025-12-19 09:56:09 -0600

    Replace Firebase config with placeholders
    Updated Firebase configuration placeholders for security.

--- firebase.js
+++ firebase.js
@@ -1,18 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Paste Firebase "Web app" config from Firebase Console -> Project settings -> Your apps
 const firebaseConfig = {
-  apiKey: "AIzaSyCA49-fMqK4vmtcVs1kvoKPsryt8otwvQA",
-  authDomain: "micds-assessments.firebaseapp.com",
-  projectId: "micds-assessments",
-  storageBucket: "micds-assessments.firebasestorage.app",
-  messagingSenderId: "104930143238",
-  appId: "1:104930143238:web:f8efeacdf297a406822787"
+  apiKey: "PASTE_YOURS",
+  authDomain: "PASTE_YOURS",
+  projectId: "PASTE_YOURS",
+  storageBucket: "PASTE_YOURS",
+  messagingSenderId: "PASTE_YOURS",
+  appId: "PASTE_YOURS"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-console.log("success");
