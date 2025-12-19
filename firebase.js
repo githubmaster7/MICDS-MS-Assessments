6dc58e969263a4b4d6fad77bc8a84f32c617737c
githubmaster7 <98905838+githubmaster7@users.noreply.github.com>
2025-12-19 10:01:08 -0600

    Update Firebase configuration with real values

--- firebase.js
+++ firebase.js
@@ -1,11 +1,17 @@
-// Paste Firebase "Web app" config from Firebase Console -> Project settings -> Your apps
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
-  authDomain: "PASTE_YOURS",
-  projectId: "PASTE_YOURS",
-  storageBucket: "PASTE_YOURS",
-  messagingSenderId: "PASTE_YOURS",
-  appId: "PASTE_YOURS"
+  apiKey: "AIzaSyCA49-fMqK4vmtcVs1kvoKPsryt8otwvQA",
+  authDomain: "micds-assessments.firebaseapp.com",
+  projectId: "micds-assessments",
+  storageBucket: "micds-assessments.firebasestorage.app",
+  messagingSenderId: "104930143238",
+  appId: "1:104930143238:web:f8efeacdf297a406822787"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
